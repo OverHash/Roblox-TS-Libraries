311e5260195be9b4cff4147aae6a40858709372a
OverHash <46231745+OverHash@users.noreply.github.com>
2020-02-07 21:56:28 +1300

    abbreviate 2.0.0

--- abbreviate/index.d.ts
+++ abbreviate/index.d.ts
@@ -1,9 +1,22 @@
 declare class abbreviator {
+	/**
+	 * Sets a setting
+	 * @param settingName The name of the setting to set
+	 * @param settingValue The value of the setting to set
+	 */
 	setSetting(settingName: 'suffixTable', settingValue: Array<string>): void;
 	setSetting(settingName: 'decimalPlaces', settingValue: number): void;
 
-	numberToString(number: number): void;
+	/**
+	 * Converts a number of full length into a string with a suffix as defined in the prefix table
+	 * @param number The number to convert into a abbreviated string
+	 */
+	numberToString(number: number): string;
 
+	/**
+	 * Converts a string that has a suffix as defined in the suffix table to a full number
+	 * @param str The string to convert into a number
+	 */
 	stringToNumber(str: string): void;
 }
 
