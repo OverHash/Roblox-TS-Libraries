46174cc638f3069772407daf468081b4e05bb409
Scyfren <67073142+Scyfren@users.noreply.github.com>
2021-03-04 11:46:10 +0800

    Add stripTrailingZeroes setting (#10)
    * fix setSetting checking truthy instead of nil
    
    * add stripTrailingZeroes setting

--- abbreviate/src/numberToString.lua
+++ abbreviate/src/numberToString.lua
@@ -1,15 +1,41 @@
+local function round(number, decimalPlaces, roundDown)
+	number = number * 10^decimalPlaces
+	if roundDown then
+		return math.floor(number) / 10^decimalPlaces
+	else
+		number = tonumber(tostring(number))
+		-- cast to string and back to number to prevent floating point errors
+		--[[ e.g.:
+			local number = 1005 / 10^3 * 10^2 + 0.5 -- 101
+			print(number, math.floor(number))
+			> 101 100
+
+			local number = 1005 / 10^3 * 10^2 + 0.5 -- 101
+			number = tonumber(tostring(number))
+			print(number, math.floor(number))
+			> 101 101
+		]]
+		return math.floor(number + 0.5) / 10^decimalPlaces
+	end
+end
+
 function numberToString(self, number, roundDown)
 	if type(number) ~= "number" then
 		error('numberToString invalid parameter #1, expected number, got "nil"', 2)
 	end
 
-	if number < 1000 and number > -1000 then
-		-- special case: we must manually abbreviate numbers between -1000 and 1000
-		return ("%."..self._decimalPlaces.."f"):format(number)
-	end
-
 	if roundDown == nil then
 		roundDown = true
+	end
+
+	if number < 1000 and number > -1000 then
+		-- special case: we must manually abbreviate numbers between -1000 and 1000
+		if not self._stripTrailingZeroes then
+			return ("%."..self._decimalPlaces.."f"):format(number)
+		else
+			number = round(number, self._decimalPlaces)
+			return tostring(number)
+		end
 	end
 
 	local negative = number < 0
@@ -20,18 +46,18 @@ function numberToString(self, number, roundDown)
 		local size = 10 ^ (index * 3)
 
 		if size <= number then
-			if roundDown then
-				number = math.floor(number * 10^self._decimalPlaces / size) / 10^self._decimalPlaces
-			else
-				number = math.floor((number * 10^self._decimalPlaces / size) + 0.5) / 10^self._decimalPlaces
-			end
+			number = round(number / size, self._decimalPlaces, roundDown)
 
 			if number == 1000 and index < #self._suffixTable[index] then
 				number = 1
 				unit = self._suffixTable[index][index + 1]
 			end
 
-			number = ("%."..self._decimalPlaces.."f"):format(number) .. unit
+			if not self._stripTrailingZeroes then
+				number = ("%."..self._decimalPlaces.."f"):format(number)
+			end
+
+			number = number .. unit
 			break
 		end
 	end
--- abbreviate/src/setSetting.lua
+++ abbreviate/src/setSetting.lua
@@ -1,13 +1,13 @@
 function setSetting(self, settingName, settingValue)
-	if not (settingName and settingValue and type(settingName) == 'string') then
+	if not (settingName and settingValue ~= nil and type(settingName) == 'string') then
 		error('setSetting had invalid parameters.\nP1 - settingName: string\nP2 - settingValue: unknown', 2)
 	end
 	local realSetting = '_' .. settingName
 
-	if self[realSetting] and type(self[realSetting]) ~= 'function' then
+	if self[realSetting] ~= nil and type(self[realSetting]) ~= 'function' then
 		self[realSetting] = settingValue
 	else
-		if self[realSetting] then
+		if self[realSetting] ~= nil then
 			error('Attempt to index setting ' ..settingName..' which is not an editable field.');
 		else
 			error('Attempt to index setting ' ..settingName..' which is not a valid setting!');
